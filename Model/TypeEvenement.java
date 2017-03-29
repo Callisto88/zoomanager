5d69799a9129c960461a317c0fa8cabe486d7b87
Balbo <cyril.balboni@gmail.com>
2017-03-29 12:45:02 +0200

    Ajout d'un constructeur pour TypeEvenement permettant de construire l'objet à partir de son ID Commentaires Javadoc Suppression du setter "setId" vu que personne est sensé pouvoir modifier l'ID depuis l'extérieur, il sera auto incrémenté au niveau du modèle lors de la création d'un nouveau type d'événement

--- Model/TypeEvenement.java
+++ Model/TypeEvenement.java
@@ -1,23 +1,56 @@
+import Model.DBInteraction;
+import Model.ExceptionDataBase;
+
+import java.sql.SQLException;
+
 /**
  * Created by D.Hamel on 26.03.17.
+ * Modified by C.Balboni on 29.3.17
  */
 public class TypeEvenement {
 
+    /**
+     * Membres privés
+     */
     private int id;
     private String type;
 
+    /**
+     * Constructeur à partir de l'identifiant, remplit automatiquement les attributs
+     * @param id un entier correspondant à l'ID du type d'événement
+     */
+    public TypeEvenement(int id) {
+        this.id = id;
+
+        // Récupération du type d'événement depuis la base de données
+        DBInteraction db = new DBInteraction();
+        try {
+            this.type = db.selTypeEvenement(id);
+        } catch (ExceptionDataBase exceptionDataBase) {
+            exceptionDataBase.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @return un entier qui correspond à l'ID du type d'événement
+     */
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     *
+     * @return une String comportant le nom du type d'événement
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * @param type String correspondant au nom du type d'événement
+     */
     public void setType(String type) {
         this.type = type;
     }
