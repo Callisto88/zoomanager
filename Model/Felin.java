23c4e4e409748f92fd93fac103d4725601b69fce
Balbo <cyril.balboni@gmail.com>
2017-04-01 19:13:58 +0200

    Constructeur avec tous les attributs, typiquement utilisé pour l'enregistrment d'un nouveau Félin

--- Model/Felin.java
+++ Model/Felin.java
@@ -1,9 +1,20 @@
 package Model;
 
+import java.util.Date;
+
 /**
  * Created by D.Hamel on 26.03.17.
  */
 public class Felin extends Animal {
 
+    private float poids;
 
+    public Felin(String nom, String sexe, Date anneeNaissance, int enclos, String origine, String race, Date dateDeces, float poids) {
+        super(nom, sexe, anneeNaissance, enclos, origine, race, dateDeces);
+        this.poids = poids;
+    }
+
+    public float getPoids() {
+        return this.poids;
+    }
 }
