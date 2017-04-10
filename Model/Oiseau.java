696d3ae9a8667680520be809d9cc5692724208cf
Balbo <cyril.balboni@gmail.com>
2017-04-10 22:27:02 +0200

    Finalisation v1.1 de la classe Oiseau

--- Model/Oiseau.java
+++ Model/Oiseau.java
@@ -1,25 +1,72 @@
 package Model;
 
+import java.util.Date;
+
 /**
- *
  * Cette classe contient la conception de la table Oiseau de la base de données
- *
+ * <p>
  * Elle contient uniquement les Setters, Getters et Constructeurs.
  *
  * @author D.Hamel
  * @author C.Balboni
- *
- * @version 1.0
- *
- * @date    28.03.2017 (Création)
- * @date    28.03.2017 (Finalisation v1.0)
- *
+ * @version 1.1
+ * @date 28.03.2017 (Création)
+ * @date 28.03.2017 (Finalisation v1.0)
+ * @date 10.04.2017 (Ajouts des constructeurs et des commentaires JavaDoc)
+ * @date 10.04.2017 (Finalisation de la v1.1
  */
-public class Oiseau {
+public class Oiseau extends Animal {
 
+    /**
+     * MEMBRES PRIVES
+     */
     private double envergure;
     private String bague;
 
+    /**
+     * Constructeurs
+     */
+
+    /**
+     * Constructeur par défaut
+     */
+    public Oiseau() {
+    }
+
+    /**
+     * Constructeur à partir de l'envergue et du no de bague de l'oiseau
+     *
+     * @param envergure
+     * @param bague
+     */
+    public Oiseau(double envergure, String bague) {
+        this.envergure = envergure;
+        this.bague = bague;
+    }
+
+    /**
+     * Constructeur avec tous les membres sauf ID
+     *
+     * @param nom
+     * @param sexe
+     * @param anneeNaissance
+     * @param enclos
+     * @param origine
+     * @param race
+     * @param dateDeces
+     */
+    public Oiseau(String nom, String sexe, Date anneeNaissance, int enclos, String origine, String race, Date dateDeces, double envergure, String bague) {
+        super(nom, sexe, anneeNaissance, enclos, origine, race, dateDeces);
+        this.envergure = envergure;
+        this.bague = bague;
+    }
+
+
+    /**
+     * GETTERS & SETTERS
+     *
+     * @return
+     */
     public double getEnvergure() {
         return envergure;
     }
