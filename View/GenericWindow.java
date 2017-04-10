d5930ef25f0059cc06e8faa330d173b070f9b517
Balbo <cyril.balboni@gmail.com>
2017-04-10 21:33:21 +0200

    GenericWindows doit être publique pour pouvoir être accessible via le package name dans les vues

--- View/GenericWindow.java
+++ View/GenericWindow.java
@@ -1,4 +1,4 @@
-package View.Stock;
+package View;
 
 import javax.swing.*;
 import java.awt.*;
@@ -6,36 +6,30 @@ import java.awt.*;
 /**
  * Created by MSilva1 on 07.04.2017.
  */
-abstract class GenericWindow extends JPanel {
-    private String windowTitle;
-    private JLabel jlErrorMessage = new JLabel();
-    private JFrame jfFrame;
-
+abstract public class GenericWindow extends JPanel {
     static private String BUTTON_FONT_NAME = "Fonte Bouton";
     static private String FONT_TITLE = "Fonte Titre";
     static private String FONT_WINDOW_TITLE = "Fonte Titre Fenêtre";
     static private String FONT_TABLE = "Fonte Table";
     static private String FONT_ERROR_MESSAGE = "Fonte Message Erreur";
-
     static private int BUTTON_FONT_SIZE = 15;
     static private int TITLE_FONT_SIZE = 30;
     static private int WINDOW_TITLE_FONT_SIZE = 15;
     static private int TABLE_FONT_SIZE = 15;
     static private int ERROR_MESSAGE_FONT_SIZE = 15;
-
+    static private int MIN_WIDTH = 1440;
+    static private int MIN_HEIGHT = 900;
+    protected JPanel jpMainPanel;
+    protected GridBagConstraints gbcMainPanel = new GridBagConstraints();
+    Dimension dim;
+    private String windowTitle;
+    private JLabel jlErrorMessage = new JLabel();
+    private JFrame jfFrame;
     private Font fButtonFont = new Font(BUTTON_FONT_NAME, Font.PLAIN, BUTTON_FONT_SIZE);
     private Font fTitleFont = new Font(FONT_TITLE, Font.PLAIN, TITLE_FONT_SIZE);
     private Font fWindowTitleFont = new Font(FONT_WINDOW_TITLE, Font.PLAIN, WINDOW_TITLE_FONT_SIZE);
     private Font fTableFont = new Font(FONT_TABLE, Font.PLAIN, TABLE_FONT_SIZE);
     private Font fErrorMessageFont = new Font(FONT_ERROR_MESSAGE, Font.PLAIN, ERROR_MESSAGE_FONT_SIZE);
-
-
-    protected JPanel jpMainPanel;
-    protected GridBagConstraints gbcMainPanel = new GridBagConstraints();
-    Dimension dim;
-
-    static private int MIN_WIDTH = 1440;
-    static private int MIN_HEIGHT = 900;
 
     public GenericWindow(String windowTitle){
         this.windowTitle = windowTitle;
@@ -49,16 +43,16 @@ abstract class GenericWindow extends JPanel {
        //gbcMainPanel.anchor = PAGE_
     }
 
+    public static int getMinWidth() {
+        return MIN_WIDTH;
+    }
+
     public String getWindowTitle(){
         return windowTitle;
     }
 
     public int getMinHeight(){
         return MIN_HEIGHT;
-    }
-
-    public static int getMinWidth() {
-        return MIN_WIDTH;
     }
 
     protected JFrame getJfFrame(){
